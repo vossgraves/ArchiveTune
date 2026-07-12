39c2579e51bf6f51e85b066764cc7edcc1658565
rukamori <itzmerinz.dc@gmail.com>
2026-07-12 18:30:47 +0700

    refactor(DownloadLibraryScreen): Import asPaddingValues

--- app/src/main/kotlin/moe/rukamori/archivetune/ui/screens/downloads/DownloadLibraryScreen.kt
+++ app/src/main/kotlin/moe/rukamori/archivetune/ui/screens/downloads/DownloadLibraryScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
